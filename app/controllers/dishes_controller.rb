d11d130619cc90c529657a58581d06a43fa30bc5
anne <richardson.ae@gmail.com>
2015-12-14 23:19:34 -0600

    added dish relationship to restaurants

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,2 +1,32 @@
 class DishesController < ApplicationController
-end
+
+  before_action :set_restaurant
+
+  def index
+    @dishes = @restaurant.dishes 
+  end #index
+    
+  def new
+    @dish = @restaurant.dishes.new
+  end #new
+
+  def create
+    @dish = @restaurant.dishes.new(dish_params)
+    #@dish.user = current_user
+    if @dish.save
+      redirect_to @restaurant, notice: "Yum! Sounds good!"
+    else
+      render :new
+    end #if
+  end #create
+
+private
+  def dish_params
+    params.require(:dish).permit(:name, :restaurant_id) 
+  end #dish_params
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end #restaurant
+    
+end #DishesController
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,13 +1,20 @@
 class Dish < ActiveRecord::Base
   belongs_to :restaurant
+
+  validates :name, presence: true, uniqueness: true
 end #Dish
 
 # == Schema Information
 #
 # Table name: dishes
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :integer          not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  restaurant_id :integer
+#
+# Indexes
+#
+#  index_dishes_on_restaurant_id  (restaurant_id)
 #
