ccdfa303772604e061aefdd075bd2553129e5a83
anne <richardson.ae@gmail.com>
2015-12-14 20:22:19 -0600

    user controller serup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,50 +25,46 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+    @user.save
+    if @user.save
+      session[:user_id] = @user.id #signs in the user upon create
+      redirect_to @user, notice: "Welcome #{@user.name}! Thanks for signing up!"
+    else
+      render :new
+    end 
+  end #create
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+    if @user.update(user_params)
+      redirect_to @user, notice: "#{@user.name}'s account has been successfully updated."
+     else
+       render :edit
+     end 
+  end #update
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+    #session[:user_id] = nil
+    redirect_to users_url, alert: "#{@user.name}'s Account successfully deleted!"
+  end #destroy
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-end
+
+    def require_correct_user
+      @user = User.find(params[:id])
+        redirect_to root_url unless current_user?(@user)
+    end #require_correct_user
+
+    
+end #UsersController
