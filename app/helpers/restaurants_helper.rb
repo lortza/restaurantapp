31c10c1cf19aea8c039b4d63458e5a382c6a7f2b
anne <richardson.ae@gmail.com>
2015-12-15 00:41:40 -0600

    image helper and added outings to restaurant show page

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,10 @@
 module RestaurantsHelper
-end
+
+  def image_for(restaurant)
+    if restaurant.image_file_name.blank?
+      image_tag "http://placehold.it/75x75"
+    else
+      image_tag restaurant.image_file_name
+    end #if
+  end #image_for(restaurant)
+end #RestaurantsHelper
