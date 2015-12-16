c85eb0d1076fba250cdb15c1a98d61870ca7e8a3
anne <richardson.ae@gmail.com>
2015-12-16 14:06:36 -0600

    added Slug to Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -65,11 +65,11 @@ class CategoriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
-      @category = Category.find(params[:id])
+      @category = Category.find_by!(slug: params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit(:name)
+      params.require(:category).permit(:name, :slug)
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,18 @@ class Category < ActiveRecord::Base
   has_many :categorizations, dependent: :destroy
   has_many :restaurants, through: :categorizations
 
+  before_validation :generate_slug
+
   validates :name, presence: true, uniqueness: true
+  validates :slug, uniqueness: true
+
+  def generate_slug
+    self.slug ||= name.parameterize if name
+  end #generate_slug
+
+  def to_param
+    slug
+  end #to_param
 
 end #Category
 
@@ -15,4 +26,5 @@ end #Category
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  slug       :string
 #
