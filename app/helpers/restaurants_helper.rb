8c19243a84bc8ce6714dba6f7381e66a5ed4b418
anne <richardson.ae@gmail.com>
2015-12-16 22:37:29 -0600

    stylized restaurant index to be more like listings

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -7,4 +7,15 @@ module RestaurantsHelper
       image_tag restaurant.image_file_name
     end #if
   end #image_for(restaurant)
+
+  ###### Wanted to make a list on categories on the restaurant index and use truncate to limit the characters
+  # def list_categories(restaurant)
+  #   category_list = []
+  #   restaurant.categories.each do |category|
+  #     category_list << category.name
+  #   end #each
+  #   truncate(category_list.to_s, length: 45, separator: " ")
+  # end #list_categories(restaurant)
+
+
 end #RestaurantsHelper
