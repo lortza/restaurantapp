b782c6514d626d72c0f5c74da8889c5786f3bd6d
anne <richardson.ae@gmail.com>
2015-12-27 20:20:38 -0600

    removed archived restaurants from categories show pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,7 +13,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
-    @restaurants = @category.restaurants
+    @restaurants = @category.restaurants.active
   end
 
   # GET /categories/new
