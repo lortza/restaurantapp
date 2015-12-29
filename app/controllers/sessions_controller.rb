1bd21552ec5f99471cc0743ed6b584521917ca13
anne <richardson.ae@gmail.com>
2015-12-28 23:58:17 -0600

    resolved root_url issue with sign in redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,23 @@
 class SessionsController < ApplicationController
+
   def new
+    if current_user.present?
+      redirect_to restaurants_url, notice: "You're already signed in! Here's your list of restaurants."
+    else
+      render :new
+    end #if
   end
 
   def create
-    if user = User.authenticate(params[:email], params[:password])
-      session[:user_id] = user.id
-      flash[:notice] = "Welcome back, #{user.name}!"
-      redirect_to restaurants_url
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       flash[:notice] = "Welcome back, #{user.name}!"
+       redirect_to root_url
     else
       flash.now[:alert] = "Invalid email/password combination"
       render :new
-    end
+    end #if
   end #create
 
   def destroy
