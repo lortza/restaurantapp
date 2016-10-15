e3b57165e94cbbe92ba92e458212960a127cc55c
anne <richardson.ae@gmail.com>
2016-10-15 11:00:40 -0500

    Implement Edit and Delete links for Dishes and Visits

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,12 +1,12 @@
 class DishesController < ApplicationController
 
   before_action :set_restaurant
-  #before_action :set_dish
+  before_action :set_dish
 
   def index
-    @dishes = @restaurant.dishes 
+    @dishes = @restaurant.dishes
   end #index
-    
+
   def new
     @dish = @restaurant.dishes.new
   end #new
@@ -21,27 +21,41 @@ class DishesController < ApplicationController
     end #if
   end #create
 
-  def show 
+  def show
   end #show
-    
+
+  def edit
+  end #edit
+
+  def update
+    respond_to do |format|
+      if @dish.update(dish_params)
+        format.html { redirect_to restaurant_path(params[:restaurant_id]), notice: 'Visit was successfully updated.' }
+        format.json { render :show, status: :ok, location: @dish }
+      else
+        format.html { render :edit }
+        format.json { render json: @dish.errors, status: :unprocessable_entity }
+      end#if
+    end#respond
+  end#update
+
   def destroy
-    @dish = @restaurant.dishes.find(params[:id]) 
+    @dish = @restaurant.dishes.find(params[:id])
     @dish.destroy
     redirect_to @restaurant
   end #destroy
-    
 
 private
   def dish_params
-    params.require(:dish).permit(:name, :restaurant_id) 
+    params.require(:dish).permit(:name, :restaurant_id)
   end #dish_params
 
   def set_restaurant
     @restaurant = Restaurant.find_by!(slug: params[:restaurant_id])
   end #restaurant
 
-  # def set_dish #this is questionable
-  #   @dish = @restaurant.dishes.find(params[:id])
-  # end #set_dish
-    
+  def set_dish
+    @dish = @restaurant.dishes.find(params[:id])
+  end #set_dish
+
 end #DishesController
--- app/controllers/outings_controller.rb
+++ app/controllers/outings_controller.rb
@@ -1,11 +1,11 @@
 class OutingsController < ApplicationController
 
-  before_action :set_restaurant
+  before_action :set_restaurant, :set_outing
 
   def index
-    @outings = @restaurant.outings 
+    @outings = @restaurant.outings
   end #index
-    
+
   def new
     @outing = @restaurant.outings.new
   end #new
@@ -20,27 +20,42 @@ class OutingsController < ApplicationController
     end #if
   end #create
 
-  def show 
+  def show
   end #show
-    
+
+  def edit
+  end #edit
+
+  def update
+    respond_to do |format|
+      if @outing.update(outing_params)
+        format.html { redirect_to restaurant_path(params[:restaurant_id]), notice: 'Visit was successfully updated.' }
+        format.json { render :show, status: :ok, location: @outing }
+      else
+        format.html { render :edit }
+        format.json { render json: @outing.errors, status: :unprocessable_entity }
+      end#if
+    end#respond
+  end#update
+
   def destroy
-    @outing = @restaurant.outings.find(params[:id]) 
+    @outing = @restaurant.outings.find(params[:id])
     @outing.destroy
     redirect_to @restaurant
   end #destroy
 
 
 private
-  
+
   def set_restaurant
     @restaurant = Restaurant.find_by!(slug: params[:restaurant_id])
   end #restaurant
 
-  # def set_outing #this is questionable
-  #   @outing = Outing.find(params[:id])
-  # end #set_outing
+  def set_outing
+    @outing = Outing.find(params[:id])
+  end #set_outing
 
   def outing_params
-    params.require(:outing).permit(:date, :note, :restaurant_id) 
+    params.require(:outing).permit(:date, :note, :restaurant_id)
   end #outing_params
 end #OutingsController
